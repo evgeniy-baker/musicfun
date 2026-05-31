90303c32329f818720dc81a47d267ad4a4290401
Evgenie Baker <evgenie_1994@mail.ru>
2026-05-31 10:39:02 +0300

    store

--- src/features/playlists/ui/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage.tsx
@@ -1,4 +1,12 @@
+import { useFetchPlaylistsQuery } from '@/features/playlists/api/PlaylistsApi.ts'
+
 export const PlaylistsPage = () => {
+  const fu = async () => {
+    console.log(await useFetchPlaylistsQuery())
+  }
+  fu()
+  // const { data } = useFetchPlaylistsQuery()
+
   return (
     <div>
       <h1>Playlists page</h1>
